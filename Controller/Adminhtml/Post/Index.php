3d3318d4103407c3951e9d9f0b01c9637ce4590d
Max Pronko <max.pronko@gmail.com>
2022-07-11 12:01:17 +0100

    Added post controller

--- Controller/Adminhtml/Post/Index.php
+++ Controller/Adminhtml/Post/Index.php
@@ -4,8 +4,20 @@ declare(strict_types=1);
 
 namespace MageMastery\Blog\Controller\Adminhtml\Post;
 
+use Magento\Backend\App\Action;
+use Magento\Framework\App\Action\HttpGetActionInterface;
+use Magento\Framework\Controller\ResultFactory;
+use Magento\Framework\View\Result\Page;
 
-class Index
+class Index extends Action implements HttpGetActionInterface
 {
+    public function execute(): Page
+    {
+        /** @var Page $resultPage */
+        $resultPage = $this->resultFactory->create(ResultFactory::TYPE_PAGE);
+        $resultPage->setActiveMenu('MageMastery_Blog::post');
+        $resultPage->getConfig()->getTitle()->prepend(__('Manage Posts'));
 
+        return $resultPage;
+    }
 }
