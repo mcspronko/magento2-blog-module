c5fc09841a8a0d95bd2c2e59c685c597a63022ab
Max Pronko <max.pronko@gmail.com>
2022-08-22 06:27:32 +0100

    Added form ui component

---
+++ Model/Post/DataProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Blog\Model\Post;
+
+use MageMastery\Blog\Model\ResourceModel\Post\CollectionFactory;
+use Magento\Ui\DataProvider\Modifier\PoolInterface;
+use Magento\Ui\DataProvider\ModifierPoolDataProvider;
+
+class DataProvider extends ModifierPoolDataProvider
+{
+    public function __construct(
+        $name,
+        $primaryFieldName,
+        $requestFieldName,
+        CollectionFactory $collectionFactory,
+        array $meta = [],
+        array $data = [],
+        PoolInterface $pool = null
+    ) {
+        parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data, $pool);
+        $this->collection = $collectionFactory->create();
+    }
+}
