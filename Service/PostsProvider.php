dcbfcc0234829064429251a4b0a23c5a09ccb3ec
Max Pronko <max.pronko@gmail.com>
2022-11-14 17:01:43 +0000

    Added loading posts from database

---
+++ Service/PostsProvider.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Blog\Service;
+
+use MageMastery\Blog\Model\ResourceModel\Post\Collection;
+use MageMastery\Blog\Model\ResourceModel\Post\CollectionFactory;
+use Magento\Framework\DB\Select;
+
+class PostsProvider
+{
+    public function __construct(private CollectionFactory $collectionFactory)
+    {}
+
+    public function getPosts(int $limit): Collection
+    {
+        $collection = $this->getCollection();
+        $collection->setOrder('creation_time', Select::SQL_DESC);
+        $collection->setPageSize($limit);
+
+        return $collection;
+    }
+
+    private function getCollection(): Collection
+    {
+        return $this->collectionFactory->create();
+    }
+}
---
+++ ViewModel/Posts.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MageMastery\Blog\ViewModel;
+
+use MageMastery\Blog\Model\ResourceModel\Post\Collection;
+use MageMastery\Blog\Service\PostsProvider;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class Posts implements ArgumentInterface
+{
+    public function __construct(private PostsProvider $postsProvider) {}
+
+    public function getPosts(int $limit): Collection
+    {
+        return $this->postsProvider->getPosts($limit);
+    }
+}
