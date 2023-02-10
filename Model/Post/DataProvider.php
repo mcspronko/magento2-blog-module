aae60ee11728587785f130b581d16c3dbd9df539
Max Pronko <max.pronko@gmail.com>
2023-02-10 12:13:32 +0000

    Added featured_image rendering support for Post UI Form

--- Model/Post/DataProvider.php
+++ Model/Post/DataProvider.php
@@ -8,7 +8,13 @@ use MageMastery\Blog\Model\Post;
 use MageMastery\Blog\Model\PostFactory;
 use MageMastery\Blog\Model\ResourceModel\Post as PostResource;
 use MageMastery\Blog\Model\ResourceModel\Post\CollectionFactory;
+use Magento\Framework\App\Filesystem\DirectoryList;
 use Magento\Framework\App\RequestInterface;
+use Magento\Framework\Filesystem\Driver\File\Mime;
+use Magento\Framework\Filesystem;
+use Magento\Framework\Filesystem\Directory\ReadInterface;
+use Magento\Framework\UrlInterface;
+use Magento\Store\Model\StoreManagerInterface;
 use Magento\Ui\DataProvider\Modifier\PoolInterface;
 use Magento\Ui\DataProvider\ModifierPoolDataProvider;
 
@@ -20,7 +26,11 @@ class DataProvider extends ModifierPoolDataProvider
     private array $loadedData;
 
     /**
-     * DataProvider constructor.
+     * @var ReadInterface 
+     */
+    private ReadInterface $mediaDirectory;
+
+    /**
      * @param $name
      * @param $primaryFieldName
      * @param $requestFieldName
@@ -28,6 +38,9 @@ class DataProvider extends ModifierPoolDataProvider
      * @param PostResource $resource
      * @param PostFactory $postFactory
      * @param RequestInterface $request
+     * @param Filesystem $filesystem
+     * @param StoreManagerInterface $storeManager
+     * @param Mime $mime
      * @param array $meta
      * @param array $data
      * @param PoolInterface|null $pool
@@ -40,16 +53,22 @@ class DataProvider extends ModifierPoolDataProvider
         private PostResource $resource,
         private PostFactory $postFactory,
         private RequestInterface $request,
+        Filesystem $filesystem,
+        private StoreManagerInterface $storeManager,
+        private Mime $mime,
         array $meta = [],
         array $data = [],
         PoolInterface $pool = null
     ) {
         parent::__construct($name, $primaryFieldName, $requestFieldName, $meta, $data, $pool);
         $this->collection = $collectionFactory->create();
+        $this->mediaDirectory = $filesystem->getDirectoryRead(DirectoryList::MEDIA);
     }
 
     /**
      * @return array
+     * @throws \Magento\Framework\Exception\FileSystemException
+     * @throws \Magento\Framework\Exception\NoSuchEntityException
      */
     public function getData(): array
     {
@@ -58,7 +77,24 @@ class DataProvider extends ModifierPoolDataProvider
         }
 
         $post = $this->getCurrentPost();
-        $this->loadedData[$post->getId()] = $post->getData();
+        $postData = $post->getData();
+
+        $image = $postData['featured_image'];
+
+        $imgDir = 'tmp/imageUploader/images';
+        $baseUrl = $this->storeManager->getStore()->getBaseUrl(UrlInterface::URL_TYPE_MEDIA);
+
+        $fullImagePath = $this->mediaDirectory->getAbsolutePath($imgDir) . '/' . $image;
+        $imageUrl = $baseUrl . $imgDir . '/' . $image;
+        $stat = $this->mediaDirectory->stat($fullImagePath);
+        
+        $postData['featured_image'] = null;
+        $postData['featured_image'][0]['url'] = $imageUrl;
+        $postData['featured_image'][0]['name'] = $image;
+        $postData['featured_image'][0]['size'] = $stat['size'];
+        $postData['featured_image'][0]['type'] = $this->mime->getMimeType($fullImagePath);
+
+        $this->loadedData[$post->getId()] = $postData;
 
         return $this->loadedData;
     }
